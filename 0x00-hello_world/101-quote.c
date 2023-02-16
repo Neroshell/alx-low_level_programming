6ff620984c2d4dbfbbc9e3bf27682d63a8d8b08d
IamNero <neroudezi@gmail.com>
2023-02-16 13:06:32 -0800

    101 quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+/**
+* main - We have the main fuction here
+*
+*Return: this has to retunr to 1 when successful
+*/
+
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
