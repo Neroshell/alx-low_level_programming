539fe65968c07f67ab6d28910196e921011084c0
IamNero <neroudezi@gmail.com>
2023-02-17 11:39:15 -0800

    printing alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+*main - main function
+*
+*Return: always 0
+*/
+
+int main(void)
+{
+
+	int a;
+
+	for (a = 'a' ; a <= 'z' ; a++)
+		putchar(a);
+	putchar('\n');
+	return (0);
+}
