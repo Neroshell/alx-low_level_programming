eca12df38b6b342094ccf0730c94ac7175dfc3f9
IamNero <neroudezi@gmail.com>
2023-02-17 12:40:46 -0800

    four print

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -5,13 +5,12 @@
 *
 *Return: would usually return 0
 */
-
 int main(void)
 {
 	char i;
 
 	for (i = 'a' ; i <= 'z' ; i++)
-		if (i != 'a' && i != 'e')
+		if (i != 'q' && i != 'e')
 			putchar(i);
 	putchar('\n');
 	return (0);
