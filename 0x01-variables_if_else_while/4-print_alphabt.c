dcc941b3fbc7bf0ad95848ed8a3223ee872bf029
IamNero <neroudezi@gmail.com>
2023-02-17 12:34:56 -0800

    4 print alphabet

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+*main - main fuction
+*
+*Return: would usually return 0
+*/
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+		if (i != 'a' && i != 'e')
+			putchar(i);
+	putchar('\n');
+	return(0);
+}
