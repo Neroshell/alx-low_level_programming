796212933b79fe49e9630cb34ee07551582bccb6
IamNero <neroudezi@gmail.com>
2023-02-20 01:58:46 -0800

    last last

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -3,37 +3,44 @@
 /**
  * main - Entry point
  *
- * Return: prints doub;e digit from 00 - 99
+i* Return: prints doub;e digit from 00 - 99
  */
 
 int main(void)
 {
+	int n, z, y, p, p1, p2;
 
-	int n, z, y, p;
-
-	for (n = 48; n <= 57 ; n++)
+	for (p = 48; p < 58 ; p++)
 	{
-		for (z =  48; z <= 56; z++)
+		for (y = 48 ; y < 57; y++)
 		{
-			for (y =  48 ; y <= 57; y++)
+			for (z =  48 ; z < 58; z++)
 			{
-				for (p = 48 ; p <= 57; p++)
-				{
-					putchar(n);
-					putchar(z);
-					putchar(' ');
-					putchar(y);
-					putchar(p);
 
-					if (n != 57 || z != 56 || y != 57 || p != 57)
+				for (n = 48 ; n < 58; n++)
+				{
+					p1 = (p * 10) + y;
+					p2 = (z * 10) + n;
+
+
+					if (p1 < p2)
 					{
+					putchar(p);
+					putchar(y);
+					putchar(' ');
+					putchar(z);
+					putchar(n);
+
+					if (p == 57 && y == 56 && z == 57 && n == 57)
+
 						break;
 					}
-						putchar(' ');
 						putchar(',');
-
+						putchar(' ');
 				}
+
 			}
+
 		}
 	}
 return (0);
