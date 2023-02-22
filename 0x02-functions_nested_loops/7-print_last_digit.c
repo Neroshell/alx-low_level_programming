24c5944b2da65147c1dd4c5845d90e2d74aa8763
IamNero <neroudezi@gmail.com>
2023-02-22 10:22:57 -0800

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* print_last_digit - function to print last digit
+*
+* @i: assumed parameters
+*
+* Return: q
+*/
+
+int print_last_digit(int i)
+{
+	int q;
+
+	q = i % 10;
+	if (i < 0)
+		q = -q;
+	return (q);
+}
